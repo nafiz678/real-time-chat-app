763132355372fba6975781d952a4dcf731477de2
MD Nafizul Iqram <nafizulikram3@gmail.com>
2025-02-11 11:36:25 +0600

    Implement validation for sending and receiving requests

--- convex/schema.ts
+++ convex/schema.ts
@@ -9,6 +9,13 @@ export default defineSchema({
         email: v.string(),
     })
         .index("by_email", ["email"])
-        .index("by_clerkId", ["clerkId"])
+        .index("by_clerkId", ["clerkId"]),
+
+    requests: defineTable({
+        sender: v.id("users"),
+        receiver: v.id("users"),
+    })
+        .index("by_receiver", ["receiver"])
+        .index("by_receiver_sender", ["receiver", "sender"]),
 
 })
